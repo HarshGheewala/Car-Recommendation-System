8d112a22bc2f5da2983beedc28ef89b94b0afbff
HarshGheewala <107661461+HarshGheewala@users.noreply.github.com>
2024-07-23 11:55:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ if st.button("Recommend"):
         }
         .car-price {
             white-space: nowrap;
-            margin-top: 10px;
+            margin-bottom:-150px
         }
         </style>
     """, unsafe_allow_html=True)
